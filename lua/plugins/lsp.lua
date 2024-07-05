9be552ee8b5fb70c762145c2dd07e14414c71347
mdnlss <rob73hall@gmail.com>
2024-07-05 01:45:31 -0400

    feat: working lsp(really)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 return {
-   {
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     lazy = true,
@@ -9,11 +9,6 @@ return {
       vim.g.lsp_zero_extend_cmp = 0
       vim.g.lsp_zero_extend_lspconfig = 0
     end,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
   },
 
   -- Autocompletion
@@ -53,42 +48,35 @@ return {
   -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    cmd = 'LspInfo',
     event = {'BufReadPre', 'BufNewFile'},
     dependencies = {
       {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason-lspconfig.nvim'},
     },
     config = function()
       -- This is where all the LSP shenanigans will live
       local lsp_zero = require('lsp-zero')
       lsp_zero.extend_lspconfig()
 
-      --- if you want to know more about lsp-zero and mason.nvim
-      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
         lsp_zero.default_keymaps({buffer = bufnr})
       end)
 
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        handlers = {
-          -- this first function is the "default handler"
-          -- it applies to every language server without a "custom handler"
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-
-          -- this is the "custom handler" for `lua_ls`
-          lua_ls = function()
-            -- (Optional) Configure lua language server for neovim
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-        }
-      })
+      -- (Optional) Configure lua language server for neovim
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+      require('lspconfig').rust_analyzer.setup(lua_opts)
+      require('lspconfig').gopls.setup(lua_opts)
+      require('lspconfig').bashls.setup(lua_opts)
+      require('lspconfig').clangd.setup(lua_opts)
+      require('lspconfig').cmake.setup(lua_opts)
+      require('lspconfig').marksman.setup(lua_opts)
+      require('lspconfig').nil_ls.setup(lua_opts)
+      require('lspconfig').zls.setup(lua_opts)
+      require('lspconfig').yamlls.setup(lua_opts)
     end
   }
 }
+
