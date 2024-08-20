a28136bffa24243e114a3edcc13deea40c8a0801
mdnlss <rob73hall@gmail.com>
2024-08-20 19:59:21 -0400

    update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,17 @@
 return {
-  'nyoom-engineering/oxocarbon.nvim',
-  -- cottmckendry/cyberdream.nvim
-  config = function()
-    vim.opt.background = "dark" -- set this to dark or light
-    vim.cmd("colorscheme oxocarbon")
-  end
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    -- cottmckendry/cyberdream.nvim
+    config = function()
+      vim.opt.background = "dark" -- set this to dark or light
+      --vim.cmd("colorscheme oxocarbon")
+    end
+  },
+  {
+    'GustavoPrietoP/doom-themes.nvim',
+    config = function()
+      vim.opt.background = "dark" -- set this to dark or light
+      vim.cmd("colorscheme doom-monokai-classic")
+    end
+  }
 }
